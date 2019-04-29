635604dc812c941ac9eff8b1ebe55f5b2467210c
booth07-mgr2 <booth07-mgr2@content.linkedin.biz>
2019-04-29 17:58:17 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import {ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { UserComponent } from './user/user.component';
+import { Routes, RouterModule } from '@angular/router';
+import { TrainComponent } from './train/train.component';
+import { ChatComponent } from './chat/chat.component';
+
+const routes: Routes = [
+  { path: 'user', component: UserComponent},
+  { path: 'train', component: TrainComponent},
+  { path: 'home', component: MainComponent},
+  { path: 'chat/:id', component: ChatComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    UserComponent,
+    TrainComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
